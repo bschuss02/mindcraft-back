64326db9c809297cbae41bee12600cfca041117a
Benji Schussheim <bas101@duke.edu>
2023-03-25 22:43:08 -0400

    barely works

--- src/routes/subRoutes.js
+++ src/routes/subRoutes.js
@@ -1,47 +1,13 @@
 const express = require("express")
 const auth = require("../middleware/authMiddleware")
 const multer = require("multer")
-const multerS3 = require("multer-s3")
 
 const subRoutes = express.Router()
-const upload = multer({
-	dest: "uploads/",
-	limits: { fileSize: 5e6 },
-	fileFilter: (req, file, cb) => {
-		const allowedMimeTypes = ["image/jpeg", "image/png", "application/pdf"]
-		if (allowedMimeTypes.includes(file.mimetype)) {
-			cb(null, true)
-		} else {
-			cb(new Error("File type not allowed"))
-		}
-	},
-	storage: multerS3({
-		s3: s3,
-		bucket: "my-bucket",
-		metadata: function(req, file, cb) {
-			cb(null, { fieldName: file.fieldname })
-		},
-		key: function(req, file, cb) {
-			cb(null, Date.now().toString() + "-" + file.originalname)
-		},
-	}),
-	// Add the boundary option
-	limits: {
-		fieldNameSize: 100,
-		fieldSize: 100 * 1024 * 1024,
-		fields: 10,
-		fileSize: 5000000,
-		files: 5,
-		parts: 15,
-		headerPairs: 100,
-		// Randomly generated UUID
-		boundary: "media-submit-boundary-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-	},
-})
+const upload = multer()
 
-subRoutes.post("/", auth, upload.array("files"), async (req, res) => {
+subRoutes.post("/", upload.any("photo"), async (req, res) => {
 	console.log("req.body", req.body)
-	console.log(req.files)
+	console.log("req.files", req.files)
 	res.send({ message: "Hello World!" })
 })
 
