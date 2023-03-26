0d50b5121ac5531288c68394c3a24cdd772ccc69
Benji Schussheim <bas101@duke.edu>
2023-03-25 23:34:14 -0400

    uploads to s3

--- src/routes/subRoutes.js
+++ src/routes/subRoutes.js
@@ -1,9 +1,36 @@
-const express = require("express")
+var express = require("express"),
+	config = require("config"),
+	aws = require("aws-sdk"),
+	bodyParser = require("body-parser"),
+	multer = require("multer"),
+	multerS3 = require("multer-s3")
+
 const auth = require("../middleware/authMiddleware")
-const multer = require("multer")
 
 const subRoutes = express.Router()
-const upload = multer()
+const aws_id = config.get("aws_id")
+const aws_secret = config.get("aws_secret")
+const aws_bucket_name = config.get("aws_bucket_name")
+
+aws.config.update({
+	secretAccessKey: aws_secret,
+	accessKeyId: aws_id,
+	region: "us-east-1", // ?
+})
+
+const s3 = new aws.S3()
+
+var upload = multer({
+	storage: multerS3({
+		s3: s3,
+		acl: "public-read",
+		bucket: aws_bucket_name,
+		key: function(req, file, cb) {
+			console.log(file)
+			cb(null, file.originalname) //use Date.now() for unique file keys
+		},
+	}),
+})
 
 subRoutes.post("/", auth, upload.any("files"), async (req, res) => {
 	console.log("req.files", req.files)
