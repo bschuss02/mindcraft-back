5a7575a289d6519415fdf562b5e68d0d6948140c
Benji Schussheim <bas101@duke.edu>
2023-03-25 23:18:37 -0400

    added check for config vars

---
+++ src/init/initConfig.js
@@ -0,0 +1,26 @@
+const config = require("config")
+
+function checkConfigs() {
+	const db = config.get("db")
+	if (!db) {
+		throw new Error("FATAL ERROR: db is not defined.")
+	}
+	const jwtPrivateKey = config.get("jwtPrivateKey")
+	if (!jwtPrivateKey) {
+		throw new Error("FATAL ERROR: jwtPrivateKey is not defined.")
+	}
+	const aws_id = config.get("aws_id")
+	if (!aws_id) {
+		throw new Error("FATAL ERROR: aws_id is not defined.")
+	}
+	const aws_secret = config.get("aws_secret")
+	if (!aws_secret) {
+		throw new Error("FATAL ERROR: aws_secret is not defined.")
+	}
+	const aws_bucket = config.get("aws_bucket_name")
+	if (!aws_bucket_name) {
+		throw new Error("FATAL ERROR: aws_bucket_name is not defined.")
+	}
+}
+
+module.exports = { checkConfigs }
